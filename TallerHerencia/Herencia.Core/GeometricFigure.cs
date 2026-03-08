7c5afc9910208fe875f5d5e368599442155a738b
fiaz7108 <fabiandiaz7108@gmail.com>
2026-03-08 17:25:59 -0500

    update

---
+++ TallerHerencia/Herencia.Core/GeometricFigure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Geometry
+{
+    public abstract class GeometricFigure
+    {
+        public string Name { get; set; }
+
+        public GeometricFigure(string name)
+        {
+            Name = name;
+        }
