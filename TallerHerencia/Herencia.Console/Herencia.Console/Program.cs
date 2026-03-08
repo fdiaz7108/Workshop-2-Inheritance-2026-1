360880a558798e028cc73f4305c6f25f2921be97
fiaz7108 <fabiandiaz7108@gmail.com>
2026-03-08 16:57:59 -0500

    update

---
+++ TallerHerencia/Herencia.Console/Herencia.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Geometry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Circle(nameof(Circle), 5);
+            var square = new Square(nameof(Square), 10);
+            var rhombus = new Rhombus(nameof(Rhombus), 5, 7, 10);
+           
