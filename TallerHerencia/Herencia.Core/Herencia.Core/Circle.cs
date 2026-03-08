40387f1f3d514365b2acea7cda3a9cc614ae69f2
fiaz7108 <fabiandiaz7108@gmail.com>
2026-03-08 17:01:39 -0500

    NUEVO

---
+++ TallerHerencia/Herencia.Core/Herencia.Core/Circle.cs
@@ -0,0 +1,38 @@
+﻿namespace Geometry
+{
+    // Clase
+    public class Circle : GeometricFigure
+    {
+        // CAMPO (Fields)
+        private double r;
+        // PROPIEDADES (properties)
+        public double R
+        {
+
+            get { return r; }
+
+            set
+            {
+                ValidateR(value);
+                r = value;
+            }
+        }
+
+        // CONSTRUCTOR (constructor)
+        public Circle(string name, double r) : base(name)
+        {
+            ValidateR(r);
+            R = r;
+        }
+
+        // METODO
+        public override double GetArea()
+        {
+            return Math.PI * R * R;
+        }
+
+        // METODO
+        public override double GetPerimeter()
+        {
+            return 2 * Math.PI * R;
+        }
